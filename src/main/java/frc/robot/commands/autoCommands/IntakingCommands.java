873f72630aeeeef7d2f5195b94c16bc13ecd7bd8
vortechs <jobartbenjamin@gmail.com>
2025-03-01 09:56:01 -0800

    untested autos

--- src/main/java/frc/robot/commands/autoCommands/IntakingCommands.java
+++ src/main/java/frc/robot/commands/autoCommands/IntakingCommands.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants;
 import frc.robot.commands.elevator.SetElevatorPresetCommand;
 import frc.robot.commands.wrist.IntakeWristCommand;
+import frc.robot.commands.wrist.SetWristRollerSpeedCommand;
 import frc.robot.commands.wrist.SetWristTargetAngleCommand;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.wrist.Wrist;
@@ -18,9 +19,18 @@ public class IntakingCommands {
         .alongWith(new IntakeWristCommand(wrist, 0.6));
   }
 
-  public static Command prepForIntakeCommand(Wrist wrist, Elevator elevator) {
-    return new SetElevatorPresetCommand(elevator, wrist, Constants.Elevator.MIN_HEIGHT)
-        .andThen(new SetWristTargetAngleCommand(wrist, () -> WristAngle.INTAKE_ANGLE.getAngle()));
+  public static Command intakeCommandAuto(Wrist wrist, Elevator elevator, int stage) {
+    double height;
+
+    if (stage == 1) {
+      height = Constants.Elevator.STAGE_2_LEVEL;
+    } else {
+      height = Constants.Elevator.STAGE_3_LEVEL;
+    }
+
+    return new SetWristTargetAngleCommand(wrist, () -> WristAngle.INTAKE_ANGLE.getAngle())
+        .andThen(new SetElevatorPresetCommand(elevator, wrist, height))
+        .andThen(new SetWristRollerSpeedCommand(wrist, 0.6));
   }
 
   public static Command prepForIntakeCommandAuto(Wrist wrist, Elevator elevator) {
--- src/main/java/frc/robot/commands/autoCommands/ScoringCommands.java
+++ src/main/java/frc/robot/commands/autoCommands/ScoringCommands.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants;
 import frc.robot.commands.elevator.SetElevatorPresetCommand;
+import frc.robot.commands.wrist.SetWristRollerSpeedCommand;
 import frc.robot.commands.wrist.SetWristTargetAngleCommand;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.wrist.Wrist;
@@ -11,25 +12,16 @@ import frc.robot.subsystems.wrist.Wrist.WristAngle;
 
 public class ScoringCommands {
 
-  public static Command prepForScoringAuto(int level, Wrist wrist, Elevator elevator) {
-    switch (level) { // bit of a misnomer here
-      case 1:
-        return new InstantCommand(() -> elevator.setTargetHeight(Constants.Elevator.STAGE_2_LEVEL))
-            .alongWith(
-                new InstantCommand(() -> wrist.setTargetAngle(WristAngle.STAGE2_ANGLE.getAngle())));
-      case 2:
-        return new InstantCommand(() -> elevator.setTargetHeight(Constants.Elevator.STAGE_3_LEVEL))
-            .alongWith(
-                new InstantCommand(() -> wrist.setTargetAngle(WristAngle.STAGE2_ANGLE.getAngle())));
-
-      case 3:
-        return new InstantCommand(() -> elevator.setTargetHeight(Constants.Elevator.BARGE_LEVEL))
-            .alongWith(
-                new InstantCommand(() -> wrist.setTargetAngle(WristAngle.STAGE2_ANGLE.getAngle())));
-
-      default:
-        return null;
-    }
+  public static Command scoreAuto(int level, Wrist wrist, Elevator elevator) {
+    return new InstantCommand(() -> wrist.setRollerSpeed(0.2), wrist)
+        .andThen(
+            new SetElevatorPresetCommand(
+                    elevator,
+                    wrist,
+                    Constants.Elevator.MAX_HEIGHT - 0.1) // just a bit less than max
+                .alongWith(
+                    new SetWristTargetAngleCommand(wrist, () -> Constants.Arm.WRIST_STAGE_4_ANGLE)))
+        .andThen(new SetWristRollerSpeedCommand(wrist, 1));
   }
 
   public static Command prepForScoring(int level, Wrist wrist, Elevator elevator) {
