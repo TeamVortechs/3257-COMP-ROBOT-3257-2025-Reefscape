0f4acce571dbde396d5429b7d5fd6be1bd189ded
vortechs <jobartbenjamin@gmail.com>
2025-03-05 18:28:48 -0800

    integrated advantage kit

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,21 +1,28 @@
 package frc.robot.subsystems.climber;
 
-import edu.wpi.first.wpilibj.DigitalInput;
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.KDoublePreferences;
 
 public class Climber extends SubsystemBase {
   private ClimberIO climberIO;
-  private ClimberIOInputsAutoLogged inputsAutoLogged = new ClimberIOInputsAutoLogged();
+  private ClimberIOInputsAutoLogged inputs = new ClimberIOInputsAutoLogged();
   // private DigitalInput limitswitch;
 
   public Climber(ClimberIO climberIO) {
     this.climberIO = climberIO;
     // this.limitswitch = limitswitch;
   }
+  
+  @Override
+  public void periodic() {
+    climberIO.updateInputs(inputs);
+    Logger.processInputs("Climber", inputs);
+  }
 
-  public void SetMotorSpeed(double speed) {
-    if(Math.abs(speed) > KDoublePreferences.PClimber.manualSpeedLimit.getValue()) {
+  public void setMotorSpeed(double speed) {
+    if (Math.abs(speed) > KDoublePreferences.PClimber.manualSpeedLimit.getValue()) {
       speed = Math.copySign(KDoublePreferences.PClimber.manualSpeedLimit.getValue(), speed);
     }
     climberIO.setMotorSpeed(speed);
