efde76b1a28fee29c6fd3787a7f2a8c1c2494caa
codeduck101 <devinl4190@gmail.com>
2025-05-17 08:45:26 -0700

    restored max height to just below previous max; commented out limelight seeking bind due to no limelight on real robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -54,7 +54,8 @@ public final class Constants {
     public static final double INTAKE_LEVEL_1 = 10.247;
     public static final double INTAKE_LEVEL_2 = 20.5;
     public static final double BARGE_LEVEL =
-        30; // currently impossible to achieve; max 44.048 without damaging; 41.7 without overshoot
+        43; // currently impossible to achieve; max 44.048 without damaging; 41.7 without
+    // overshoot
   }
 
   // constants used for the arm/wrist (naming inconsistency)
