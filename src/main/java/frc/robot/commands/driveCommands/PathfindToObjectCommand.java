ebff1952807d4d5a6360ac92f34a11ebc033b946
vortechs <jobartbenjamin@gmail.com>
2025-11-22 13:48:28 -0800

    slight tweak

--- src/main/java/frc/robot/commands/driveCommands/PathfindToObjectCommand.java
+++ src/main/java/frc/robot/commands/driveCommands/PathfindToObjectCommand.java
@@ -219,7 +219,7 @@ public class PathfindToObjectCommand extends Command {
         thetaController.calculate(
             currentPose.getRotation().getRadians(), targetPose.getRotation().getRadians());
 
-    if (totalDist < 2 && Math.abs(thetaDistance) > 0.2) {
+    if (totalDist < 2.3 && Math.abs(thetaDistance) > 0.2) {
       xVelocity = 0;
       yVelocity = 0;
     }
