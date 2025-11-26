9d6a051886bbf7f2b973f9fd3e7b3cf8f78450b8
sylin54 <104856945+sylin54@users.noreply.github.com>
2025-11-25 20:06:21 -0800

    added photonvision test cdoe

---
+++ src/main/java/frc/robot/subsystems/vision/Detection/DetectionIOPhotonVision.java
@@ -0,0 +1,150 @@
+package frc.robot.subsystems.vision.Detection;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.BaseUnits;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.RawDetection;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.vision.VisionConstants;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class DetectionIOPhotonVision implements DetectionIO {
+
+//the camera
+  private PhotonCamera camera;
+
+  // drive system
+  private Drive drive;
+
+  // the id of the object that is tracking
+  private int objectID;
+
+  // the cloest field object
+  private FieldObject closestDetection = null;
+
+  // outputs
+  private Pose2d closestDetectionPose = null;
+  private boolean isDetected = false;
+
+  public DetectionIOPhotonVision(String name, Drive drive, int objectID) {
+    camera = new PhotonCamera(name);
+
+    this.drive = drive;
+    this.objectID = objectID;
+
+  }
+
+  @Override
+  public void updateInputs(DetectionIOInputsAutoLogged inputsAutoLogged) {
+    inputsAutoLogged.closestDetectionPose = closestDetectionPose;
+    inputsAutoLogged.isDetected = isDetected;
+
+    inputsAutoLogged.numberOfObjects = 0;
+
+    if (!isDetected) {
+      inputsAutoLogged.objectDistance = -1;
+    }
+    inputsAutoLogged.objectDistance =
+        closestDetectionPose.getTranslation().getDistance(drive.getPose().getTranslation());
+  }
+
+  // interface methods
+
+  @Override
+  public void update() {
+
+    double curTime = Timer.getFPGATimestamp();
+
+
+
+    List<PhotonPipelineResult> results = camera.getAllUnreadResults();
+
+    if(results.isEmpty()) {
+        isDetected = false;
+        return;
+    }
+
+    PhotonPipelineResult result = results.get(0);
+
+    if(!result.hasTargets()) {
+        isDetected = false;
+        return;
+    }
+
+    isDetected = true;
+
+    PhotonTrackedTarget pTarget = result.getBestTarget();
+    Transform3d offset = pTarget.getBestCameraToTarget();
+
+    Translation2d pose = new Translation2d(offset.getMeasureX(), offset.getMeasureY());
+
+    FieldObject obj = new FieldObject(pose, curTime);
+
+    closestDetection = obj;
+
+
+
+  }
+
+  /**
+   * gets the rotation that the robot needs to be to pick this up. PERHAPS THIS NEEDS TX TO WORK
+   * WELL. EXPERIMENT
+   */
+  @Override
+  public Rotation2d getHeading() {
+
+    if (!isDetected()) return new Rotation2d();
+
+    Translation2d delta = closestDetection.position.minus(drive.getPose().getTranslation());
+
+    return new Rotation2d(delta.getX(), delta.getY());
+  }
+
+  /**
+   * Gets the position of the object. Use isDetected first
+   *
+   * <p>If no object is detected it will return the position of the robot
+   */
+  @Override
+  public Pose2d getObjectPosition() {
+    return closestDetectionPose;
+  }
+
+  /**
+   * Gets wether or not the robot detects the object. This should always be used before get object
+   * position
+   */
+  @Override
+  public boolean isDetected() {
+    return isDetected;
+  }
+
+
+  // how we define an object that we are detecting. Data holder
+  public static class FieldObject {
+    Translation2d position;
+    double detectionTime;
+
+    public FieldObject(Translation2d position, double detectionTime) {
+      this.position = position;
+      this.detectionTime = detectionTime;
+    }
+  }
+
+}
